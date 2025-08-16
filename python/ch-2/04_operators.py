8cb8acfbc3fb7bd7d016619f400329027d7e5a15
bug2905 <skushal2905@gmail.com>
2025-08-16 12:20:27 +0530

    operators

---
+++ python/ch-2/04_operators.py
@@ -0,0 +1,44 @@
+# Arithmetic Operators
+a = 4
+b = 24
+c = a + b
+print(c)
+
+# Assignment Operators
+a = 4 - 2 # Assign 4-3 in a
+print(a)
+b = 6 
+print("value before increment " , b)
+b += 3 # Increment the value of b by 3 and assign it to b
+print("value after increment " , b)
+b -= 3 # Decrement the value of b by 3 and assign it to b
+print("value after Decrement ", b)
+
+# Logical Operators
+# that return true or false
+d = 5 < 3
+print(d)
+e = 5 == 5
+print(e)
+
+# Logical Operators
+
+f = True or False
+print("--------------------------")
+print("----Truth Table of or ----")
+print("--------------------------")
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+print("--------------------------")
+print("----Truth Table of and----")
+print("--------------------------")
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+print("--------------------------")
+
+print(not(False))# give oposite output of boole
