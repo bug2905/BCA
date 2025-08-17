d545697e6dcd1df0769d2130e2e04f4de1692569
bug2905 <skushal2905@gmail.com>
2025-08-17 12:00:43 +0530

    string and slicing

--- python/ch-3/01_intro_to_string.py
+++ python/ch-3/01_intro_to_string.py
@@ -1,4 +1,6 @@
 name = "kushal"
 
-nameshort = name[1:4]
-print(nameshort)
+nameshort = name[1:4] #o/p :-ush
+nameshort = name[:4] #o/p :-kush
+nameshort = name[0:] #o/p :-kushal
+nameshort = name[0:4:2] #o/p :- ks
