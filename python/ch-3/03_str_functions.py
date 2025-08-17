6bfe875c8e13d742c0f6945ad9d9068b20243055
bug2905 <skushal2905@gmail.com>
2025-08-17 12:41:20 +0530

    string function

---
+++ python/ch-3/03_str_functions.py
@@ -0,0 +1,11 @@
+name = "kushal"
+
+print(len(name))#return length of string or given variable
+print(name.endswith("al"))#return true or false if string end with this sub string
+print(name.startswith("k"))#return true or false if string start with this sub string
+print(name.upper())      # Converts string to uppercase.
+print(name.lower())      # Converts string to lowercase.
+print(name.title())      # Converts first letter of each word to uppercase
+print(name.capitalize()) # Converts first character to uppercase, rest lowercase
+print(name.swapcase())   # Swaps uppercase ↔ lowercase.
+
