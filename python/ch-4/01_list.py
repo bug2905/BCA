a3701cd3fefecb14b4e10aded18a22e53b327537
bug2905 <skushal2905@gmail.com>
2025-08-17 15:39:12 +0530

    list and it's methods

---
+++ python/ch-4/01_list.py
@@ -0,0 +1,5 @@
+friends = ["apple","dhruv","tirth","falguni",7,False]
+
+print(friends[0])
+friends[0]="grapes" #unlike strings lists are mutable
+print(friends[0])
---
+++ python/ch-4/02_list_method.py
@@ -0,0 +1,20 @@
+friends = ["apple","dhruv","tirth","falguni",7,False]
+
+print(friends)
+friends.append("kushal")
+print(friends)
+
+l1 = [1,34,67,38,0,2]
+l2 = [1,34,67,38,0,2]
+l3 = [1,34,67,38,0,2]
+l4 = [1,34,67,38,0,2]
+l1.sort()
+print(l1)
+l2.reverse()
+print(l2)
+l3.insert(3,3333)
+print(l3)
+l4.pop(3)
+print(l4)
+
+
