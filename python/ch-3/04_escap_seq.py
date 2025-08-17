d12806cbc6f3ea3ddd9096cd4f9cd76ae75a82db
bug2905 <skushal2905@gmail.com>
2025-08-17 12:52:26 +0530

    escap sequences

---
+++ python/ch-3/04_escap_seq.py
@@ -0,0 +1,6 @@
+# \n is use to break lines or display content in new line
+a = "kushal is a good boy \n but not a bad boy"
+print(a)
+# \t give 1 tab space in terminal or output
+b = "print using \\t \n\t in output"
+print(b)
