ace1fd12ac6567949dd50a8827b5dae7c91623c9
bug2905 <skushal2905@gmail.com>
2025-08-19 11:58:45 +0530

    changes

--- python/ch-4/03_tuple.py
+++ python/ch-4/03_tuple.py
@@ -1,4 +1,6 @@
-a = (1,2,3,4,5,6)
+a = (1,2,3,4,5,6) #tuple with more than one element
 print(type(a))
-b = ()
+b = () #empty tuple
 print(type(b))
+c = (1,) #tuple with only one element need comma
+print(type(c))
