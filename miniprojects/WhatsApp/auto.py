b54f611405de4e3a083bd5f0d73d2e8ad77a5a82
bug2905 <skushal2905@gmail.com>
2025-09-20 10:38:55 +0530

    small changes

--- miniprojects/WhatsApp/auto.py
+++ miniprojects/WhatsApp/auto.py
@@ -1,4 +1,4 @@
 import pywhatkit
 
 #Send message at a specific time
-pywhatkit.sendwhatmsg("+916356311541", "Hello! This is an automated message.",21,59)
+pywhatkit.sendwhatmsg("+916356311541", "Hello! This is an automated message.",9,59)
